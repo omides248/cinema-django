a3ca1d0c202c08e7e44120c53ff19e6471212e72
omid <omidesmaeeli4@gmail.com>
2018-09-07 20:16:58 +0430

    Translate the foregin keys of the screening app

--- screening/models.py
+++ screening/models.py
@@ -4,12 +4,16 @@ from ckeditor.fields import RichTextField
 from django.utils.translation import ugettext_lazy as _
 from django_jalali.db import models as jmodels
 
+CHAIR_STATUS = (
+	(0 , _('Not reserved chair')),
+	(1 , _('Reserved'))
+)
 
 class Chair(models.Model):
 	chair_number 				= models.CharField(max_length=3, verbose_name=_("name"))
-	status 						= models.BooleanField()
-	order  						= models.ForeignKey("customers.Order", on_delete=models.CASCADE)
-	screening   				= models.ForeignKey("Screening", on_delete=models.CASCADE)
+	status 						= models.BooleanField(choices=CHAIR_STATUS, default=0, verbose_name=_("status"))
+	order  						= models.ForeignKey("customers.Order", on_delete=models.CASCADE, verbose_name=_("order"))
+	screening   				= models.ForeignKey("Screening", on_delete=models.CASCADE, verbose_name=_("screening"))
 
 	def __str__(self):
 		return self.chair_number
@@ -23,8 +27,8 @@ class Screening(models.Model):
 	name 				= models.CharField(max_length=128, verbose_name=_("name"))
 	price 				= models.IntegerField(verbose_name=_("price"))
 	time_screening 		= jmodels.jDateTimeField(verbose_name=_("time_screening"))
-	hall 				= models.ForeignKey("Hall", on_delete=models.CASCADE)
-	cinema    			= models.ForeignKey("Cinema", on_delete=models.CASCADE)
+	hall 				= models.ForeignKey("Hall", on_delete=models.CASCADE, verbose_name=_("hall"))
+	cinema    			= models.ForeignKey("Cinema", on_delete=models.CASCADE, verbose_name=_("cinema"))
 
 	def __str__(self):
 		return self.name
@@ -50,7 +54,7 @@ class Cinema(models.Model):
 	city 				= models.CharField(max_length=128, verbose_name=_("city"))
 	address				= RichTextField(max_length=128, verbose_name=_("description_city"))
 	phone_number		= PhoneNumberField(verbose_name=_("phone_number"))
-	movie  				= models.ForeignKey("movies.Movie", on_delete=models.CASCADE)
+	movie  				= models.ForeignKey("movies.Movie", on_delete=models.CASCADE, verbose_name=_("movie"))
 
 	def __str__(self):
 		return self.name
