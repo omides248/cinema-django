12d49281adf780692cda57a61b868bb099ae3b49
omid <omidesmaeeli4@gmail.com>
2018-09-09 00:13:56 +0430

    Add AbastractUser class to User model in customers app

--- customers/admin.py
+++ customers/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from customers.models import Order
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
-# admin.site.register(Customer)
+admin.site.register(User, UserAdmin)
 admin.site.register(Order)
