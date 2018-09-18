bd212b1b5e3e9bec417c01e234e585fa49673d8e
omid <omidesmaeeli4@gmail.com>
2018-09-19 04:25:50 +0430

    Add password forget field

--- login_register/forms.py
+++ login_register/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from phonenumber_field.formfields import PhoneNumberField
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field, Fieldset
 from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 
 
@@ -16,6 +16,7 @@ class RegisterForm(forms.Form):
     phone_number = PhoneNumberField(label=_('phone_number'))
     email = forms.EmailField(label=_('email'))
     password = forms.CharField(widget=forms.PasswordInput, label=_('password'))
+    password_reapet = forms.CharField(widget=forms.PasswordInput, label=_('password_reapet'))
 
 
 
@@ -29,9 +30,9 @@ class RegisterForm(forms.Form):
         Field('email', placeholder=_("Enter your email address")),
 
         AppendedText('password', '<a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>', placeholder=_("Enter your password"), id="basic-addon1"),
-        AppendedText('password', '<a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>', placeholder=_("Enter your repeat password"), id="basic-addon1"),
+        AppendedText('password_reapet', '<a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>', placeholder=_("Enter your repeat password"), id="basic-addon1"),
         FormActions(
-            Submit('register_form', _('Register'), css_class="btn-success"),
+            Submit('register_form', _('Register'), css_class="btn-info"),style="margin: auto; width: -moz-fit-content;"
         )
     )
 
@@ -42,6 +43,7 @@ class LoginForm(forms.Form):
 
     email = forms.EmailField(label=_('email'))
     password = forms.CharField(widget=forms.PasswordInput, label=_('password'))
+    checkbox_remember = forms.BooleanField(label=_('checkbox remember'), required = False, initial=True)
 
     helper = FormHelper()
     helper.form_class = 'form-vertical'
@@ -51,7 +53,10 @@ class LoginForm(forms.Form):
 
         AppendedText('password', '<a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>', placeholder=_('Enter your password')),
 
+        Field('checkbox_remember'),
+# margin: auto;text-align: center;width: 50%;
         FormActions(
-        Submit('login_form', _("Login"), css_class="btn-success")
-        )
+        Submit('login_form', _("Login"), css_class="btn-success"), style="margin: auto; width: -moz-fit-content;"
+        ),
+        HTML("""<p id="title-forget-password"><a href="#" id="forget-password">فراموشی رمز عبور</a></p>"""),
     )
